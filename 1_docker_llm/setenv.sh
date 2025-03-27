990a0f97988888365b5490cca69d688e4f2a7832
Airren <qiang.ren@intel.com>
2025-03-27 14:34:02 +0800

    feat: add gpu affinity for multi-gpu
    Signed-off-by: Airren <qiang.ren@intel.com>

--- 1_docker_llm/setenv.sh
+++ 1_docker_llm/setenv.sh
@@ -9,17 +9,19 @@ export MODELS_PATH=$(pwd)/../models
 
 
 # set the name of model used for serving
-export LLM_MODEL_ID=deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B
+export LLM_MODEL_ID=deepseek-ai/DeepSeek-R1-Distill-Qwen-32B
 # set the path where locates the pre-downloaded model corresponding to the LLM_MODEL_ID
 export LLM_MODEL_LOCAL_PATH=/data/${LLM_MODEL_ID}
 
-export SHM_SIZE="8g"
+export SHM_SIZE="32g"
 
 export DTYPE=float16
 export QUANTIZATION=fp8
 export MAX_MODEL_LEN=2048
 export MAX_NUM_BATCHED_TOKENS=4000
 export MAX_NUM_SEQS=256
-export TENSOR_PARALLEL_SIZE=1
+export TENSOR_PARALLEL_SIZE=4
+export GPU_AFFINITY="0,1,2,3"
+
 
 
